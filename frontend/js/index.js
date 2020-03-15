cb83ba8270b0f09f84e479dfd410c199db01244d
Vasili Skurydzin <vasili.skurydzin@mail.utoronto.ca>
2020-03-14 20:31:58 -0400

    wrapped index js code in closure

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,40 +1,41 @@
 /*jshint esversion: 6 */
-// Sets Up the Add Stock Bar at top of page
-$("#singleSearch").select2({
-    placeholder: 'Select A Stock',
-    theme: "flat",
-    allowClear: true,
-    ajax: {
-        url:'https://financialmodelingprep.com/api/v3/search?query=&limit=10',
-        processResults: function (ajaxData){
-            let data = $.map(ajaxData, function (obj,index){
-                obj.id = obj.id || index + 1;
-                obj.text = obj.text || obj.symbol; // replace name with the property used for the text
-                return obj;
-              });
-              
-            return {results: data};
-        },
-        cache: true
-    }
-});
+window.onload = (function(){
+    "use strict";
 
-// Behavoiour For when a stock is selected from the drop down list
-$("#singleSearch").on('select2:select', function (e) {
-    let data = e.params.data;
-    let tr = document.createElement('tr');
-    tr.innerHTML = `<td class="tm-product-name">${data.name}</td>
-    <td class="text-center">
-        <a class="tm-product-delete-link">
-            <i class="far fa-trash-alt tm-product-delete-icon"></i>
-        </a>
-    </td>`;
-    $('#StockSelections').prepend(tr);
-    tr.querySelector('i').addEventListener('click',function(e){
-       tr.parentElement.removeChild(tr);
+    // Sets Up the Add Stock Bar at top of page
+    $("#singleSearch").select2({
+        placeholder: 'Select A Stock',
+        theme: "flat",
+        allowClear: true,
+        ajax: {
+            url:'https://financialmodelingprep.com/api/v3/search?query=&limit=10',
+            processResults: function (ajaxData){
+                let data = $.map(ajaxData, function (obj,index){
+                    obj.id = obj.id || index + 1;
+                    obj.text = obj.text || obj.symbol; // replace name with the property used for the text
+                    return obj;
+                 });
+                 return {results: data};
+            },
+            cache: true
+        }
     });
-    $("#singleSearch").val(null).trigger("change");
 
-});
+    // Behavoiour For when a stock is selected from the drop down list
+    $("#singleSearch").on('select2:select', function (e) {
+        let data = e.params.data;
+        let tr = document.createElement('tr');
+        tr.innerHTML = `<td class="tm-product-name">${data.name}</td>
+        <td class="text-center">
+            <a class="tm-product-delete-link">
+                <i class="far fa-trash-alt tm-product-delete-icon"></i>
+            </a>
+        </td>`;
+        $('#StockSelections').prepend(tr);
+        tr.querySelector('i').addEventListener('click',function(e){
+           tr.parentElement.removeChild(tr);
+        });
+        $("#singleSearch").val(null).trigger("change");
 
-
+    });
+})();
