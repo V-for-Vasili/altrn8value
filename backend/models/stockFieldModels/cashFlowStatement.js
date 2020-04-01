e432894c9b8ff0e9eb0a3861a6670290461f99b0
Vasili Skurydzin <vasili.skurydzin@mail.utoronto.ca>
2020-03-31 22:53:05 -0400

    Refactoring cashFlowStatement.js

--- backend/models/stockFieldModels/cashFlowStatement.js
+++ backend/models/stockFieldModels/cashFlowStatement.js
@@ -22,18 +22,7 @@ type CashFlowStatement {
     net_cash_marketcap: String
   }
 `
-
-let cashFlowStatementYearResolver = async (obj, args, context, info) => {
-  let response = {};
-  try {
-    response = await axios.get(`https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/${obj.symbol}`);
-    response = response.data.financials;
-  } catch (err) {
-    console.log(err);
-  }
-  if(!response) {
-    return null;
-  } else {
+function parse_response(response) {
     let statements = [];
     response.forEach(function(item) {
         statements.push({
@@ -56,6 +45,20 @@ let cashFlowStatementYearResolver = async (obj, args, context, info) => {
         });
     });
     return statements;
+}
+
+let cashFlowStatementYearResolver = async (obj, args, context, info) => {
+  let response = {};
+  try {
+    response = await axios.get(`https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/${obj.symbol}`);
+    response = response.data.financials;
+  } catch (err) {
+    console.log(err);
+  }
+  if(!response) {
+    return null;
+  } else {
+    return parse_response(response);
   }
 }
 
@@ -70,28 +73,7 @@ let cashFlowStatementQuarterResolver = async (obj, args, context, info) => {
   if(!response) {
     return null;
   } else {
-    let statements = [];
-    response.forEach(function(item) {
-        statements.push({
-            date: item['date'],
-            depreciation_and_amortization: item['Depreciation & Amortization'],
-            stock_based_compensation: item['Stock-based compensation'],
-            operating_cash_flow: item['Operating Cash Flow'],
-            capital_expenditure: item['Capital Expenditure'],
-            acquisitions_and_disposals: item['Acquisitions and disposals'],
-            investment_purchases_and_sales: item['Investment purchases and sales'],
-            investing_cash_flow: item['Investing Cash flow'],
-            issuance_repayment_of_debt: item['Issuance (repayment) of debt'],
-            issuance_buybacks_of_shares: item['Issuance (buybacks) of shares'],
-            dividend_payments: item['Dividend payments'],
-            financing_cash_flow: item['Financing Cash Flow'],
-            effect_of_forex_changes_on_cash: item['Effect of forex changes on cash'],
-            net_cash_flow_change_in_cash: item['Net cash flow / Change in cash'],
-            free_cash_flow: item['Free Cash Flow'],
-            net_cash_marketcap: item['Net Cash/Marketcap'],
-        });
-    });
-    return statements;
+    return parse_response(response);
   }
 }
 
