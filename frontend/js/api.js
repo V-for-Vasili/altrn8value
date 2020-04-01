75aee1b37f9ab214d740a82b12dcc14c6ea3d392
Vasili Skurydzin <vasili.skurydzin@mail.utoronto.ca>
2020-04-01 14:55:49 -0400

    refactor api to store metrics for the data we fetch from backend

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -62,6 +62,122 @@ let api = (function(){
         module.notifyLoginListeners();
     }
 
+    // helper f-on to get all metric labels; Omits 'date'
+    function get_metric_names(obj) {
+        let names = [];
+        Object.keys(obj).forEach(function(key) {
+            if (key === 'date') return;
+            names.push(obj[key]);
+        });
+        return names;
+    }
+
+    //
+    // Declarations of the metrics we fetch from the backend
+    //
+
+    // years for which we show financial statements
+    module.years = ['2019', '2018', '2017', '2016', '2015', '2014'];
+
+    module.incomeStatementMetrics = {
+        date: 'date',
+        revenue: 'Revenue',
+        revenue_growth: 'Revenue Growth',
+        cost_of_revenue: 'Cost of Revenue',
+        gross_profit: 'Gross Profit',
+        rd_expenses: 'R&D Expenses',
+        sga_expense: 'SG&A Expense',
+        operating_expenses: 'Operating Expenses',
+        operating_income: 'Operating Income',
+        interest_expense: 'Interest Expense',
+        earnings_before_tax: 'Earnings before Tax',
+        income_tax_expense: 'Income Tax Expense',
+        net_ncome_non_controlling_int: 'Net Income - Non-Controlling int',
+        net_income_discontinued_ops: 'Net Income - Discontinued ops',
+        net_income: 'Net Income',
+        preferred_dividends: 'Preferred Dividends',
+        net_income_com: 'Net Income Com',
+        eps: 'EPS',
+        eps_diluted: 'EPS Diluted',
+        weighted_average_shs_out: 'Weighted Average Shs Out',
+        weighted_average_shs_out_dil: 'Weighted Average Shs Out (Dil)',
+        dividend_per_share: 'Dividend per Share',
+        gross_margin: 'Gross Margin',
+        ebitda_margin: 'EBITDA Margin',
+        ebit_margin: 'EBIT Margin',
+        profit_margin: 'Profit Margin',
+        free_cash_flow_margin: 'Free Cash Flow margin',
+        ebitda: 'EBITDA',
+        ebit: 'EBIT',
+        consolidated_income: 'Consolidated Income',
+        earnings_before_tax_margin: 'Earnings Before Tax Margin',
+        net_profit_margin: 'Net Profit Margin',
+    };
+
+    module.balanceSheetMetrics = {
+        date: 'date',
+        cash_and_cash_equivalents: 'Cash and cash equivalents',
+        short_term_investments: 'Short-term investments',
+        cash_and_short_term_investments: 'Cash and short-term investments',
+        receivables: 'Receivables',
+        inventories: 'Inventories',
+        total_current_assets: 'Total current assets',
+        property_plant_and_equipment_net: 'Property, Plant & Equipment Net',
+        goodwill_and_intangible_assets: 'Goodwill and Intangible Assets',
+        long_term_investments: 'Long-term investments',
+        tax_assets: 'Tax assets',
+        total_non_current_assets: 'Total non-current assets',
+        total_assets: 'Total assets',
+        payables: 'Payables',
+        short_term_debt: 'Short-term debt',
+        total_current_liabilities: 'Total current liabilities',
+        long_term_debt: 'Long-term debt',
+        total_debt: 'Total debt',
+        deferred_revenue: 'Deferred revenue',
+        tax_liabilities: 'Tax Liabilities',
+        deposit_liabilities: 'Deposit Liabilities',
+        total_non_current_liabilities: 'Total non-current liabilities',
+        total_liabilities: 'Total liabilities',
+        other_comprehensive_income: 'Other comprehensive income',
+        retained_earnings_deficit: 'Retained earnings (deficit)',
+        total_shareholders_equity: 'Total shareholders equity',
+        investments: 'Investments',
+        net_debt: 'Net Debt',
+        other_ssets: 'Other Assets',
+        other_liabilities: 'Other Liabilities',
+    };
+
+    module.cashFlowStmtMetrics = {
+        date: 'date',
+        depreciation_and_amortization: 'Depreciation & Amortization',
+        stock_based_compensation: 'Stock-based compensation',
+        operating_cash_flow: 'Operating Cash Flow',
+        capital_expenditure: 'Capital Expenditure',
+        acquisitions_and_disposals: 'Acquisitions and disposals',
+        investment_purchases_and_sales: 'Investment purchases and sales',
+        investing_cash_flow: 'Investing Cash flow',
+        issuance_repayment_of_debt: 'Issuance (repayment) of debt',
+        issuance_buybacks_of_shares: 'Issuance (buybacks) of shares',
+        dividend_payments: 'Dividend payments',
+        financing_cash_flow: 'Financing Cash Flow',
+        effect_of_forex_changes_on_cash: 'Effect of forex changes on cash',
+        net_cash_flow_change_in_cash: 'Net cash flow / Change in cash',
+        free_cash_flow: 'Free Cash Flow',
+        net_cash_marketcap: 'Net Cash/Marketcap',
+    };
+
+    module.getIncomeStatementMetricNames = function() {
+        return get_metric_names(module.incomeStatementMetrics);
+    };
+
+    module.getBalanceSheetMetricNames = function() {
+        return get_metric_names(module.balanceSheetMetrics);
+    };
+
+    module.getCashFlowStmtMetricNames = function() {
+        return get_metric_names(module.cashFlowStmtMetrics);
+    };
+
     //
     // authentication
     //
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -131,89 +131,10 @@ window.onload = (function(){
         }
     }
 
-    // years for which we show financial statements
-    let years = ['2019', '2018', '2017', '2016', '2015', '2014'];
-    let incomeStatementMetrics = [
-        'Revenue',
-        'Revenue Growth',
-        'Cost of Revenue',
-        'Gross Profit',
-        'R&D Expenses',
-        'SG&A Expense',
-        'Operating Expenses',
-        'Operating Income',
-        'Interest Expense',
-        'Earnings before Tax',
-        'Income Tax Expense',
-        'Net Income - Non-Controlling int',
-        'Net Income - Discontinued ops',
-        'Net Income',
-        'Preferred Dividends',
-        'Net Income Com',
-        'EPS',
-        'EPS Diluted',
-        'Weighted Average Shs Out',
-        'Weighted Average Shs Out (Dil)',
-        'Dividend per Share',
-        'Gross Margin',
-        'EBITDA Margin',
-        'EBIT Margin',
-        'Profit Margin',
-        'Free Cash Flow margin',
-        'EBITDA',
-        'EBIT',
-        'Consolidated Income',
-        'Earnings Before Tax Margin',
-        'Net Profit Margin'
-    ];
-    let balanceSheetMetrics = [
-        "Cash and cash equivalents",
-        "Short-term investments",
-        "Cash and short-term investments",
-        "Receivables",
-        "Inventories",
-        "Total current assets",
-        "Property, Plant & Equipment Net",
-        "Goodwill and Intangible Assets",
-        "Long-term investments",
-        "Tax assets",
-        "Total non-current assets",
-        "Total assets",
-        "Payables",
-        "Short-term debt",
-        "Total current liabilities",
-        "Long-term debt",
-        "Total debt",
-        "Deferred revenue",
-        "Tax Liabilities",
-        "Deposit Liabilities",
-        "Total non-current liabilities",
-        "Total liabilities",
-        "Other comprehensive income",
-        "Retained earnings (deficit)",
-        "Total shareholders equity",
-        "Investments",
-        "Net Debt",
-        "Other Assets",
-        "Other Liabilities"
-    ];
-    let getCashFlowStmtMetrics = [
-        "Depreciation & Amortization",
-        "Stock-based compensation",
-        "Operating Cash Flow",
-        "Capital Expenditure",
-        "Acquisitions and disposals",
-        "Investment purchases and sales",
-        "Investing Cash flow",
-        "Issuance (repayment) of debt",
-        "Issuance (buybacks) of shares",
-        "Dividend payments",
-        "Financing Cash Flow",
-        "Effect of forex changes on cash",
-        "Net cash flow / Change in cash",
-        "Free Cash Flow",
-        "Net Cash/Marketcap"
-    ];
+    let years = api.years;
+    let incomeStatementMetrics = api.getIncomeStatementMetricNames();
+    let balanceSheetMetrics = api.getBalanceSheetMetricNames();
+    let cashFlowStmtMetrics = api.getCashFlowStmtMetricNames();
 
     function formatState (state) {
         if (!state.id) {
@@ -454,7 +375,7 @@ window.onload = (function(){
     }
 
     function metricsTableShowCashFlowStatements(companyName) {
-        populateMetricsTable(api.getCashFlowStmt, companyName, getCashFlowStmtMetrics, "Cash Flow Statement");
+        populateMetricsTable(api.getCashFlowStmt, companyName, cashFlowStmtMetrics, "Cash Flow Statement");
     }
 
     function metricsTableChangeCurrStock(symbol) {
