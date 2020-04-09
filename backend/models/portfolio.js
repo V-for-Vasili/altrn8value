507de4ca97399d4b4be48c7103f8264866d93b37
Vasili Skurydzin <vasili.skurydzin@mail.utoronto.ca>
2020-04-09 12:37:28 -0400

    Backend cleanup + consistent indent for readability

--- backend/models/portfolio.js
+++ backend/models/portfolio.js
@@ -95,90 +95,88 @@ let createPortfolioResolver = async (obj, args, context, info) => {
 let portfolioQueryDef = `portfolio(name: String!): Portfolio`;
 
 let portfolioQueryResolver = async (obj, args, context, info) => {
-  // Check the user is authenticated
-  if (!context.isAuth) throw new Error(errorName.ACCESS_DENIED);
-  let userID = context.uid;
-  //let userID = "sanicTheHedgehog";
-  // Get Portfolio by name from db
-  let result = null;
-  let response = await Portfolio.findOne({uid: userID, name: args.name}, function(err, res) {
-    if (err) return console.log(err);
-    if(!res) throw new Error(errorName.NOT_FOUND);
-    res.stock_list = res.stock_list.map(obj => {
-      let rObj = obj;
-      rObj.stock = {};
-      rObj.stock.symbol = obj.symbol;
-      rObj.purchaseTime = rObj.purchaseTime.toJSON();
-      return rObj;
+    // Check the user is authenticated
+    if (!context.isAuth) throw new Error(errorName.ACCESS_DENIED);
+    let userID = context.uid;
+    // Get Portfolio by name from db
+    let result = null;
+    let response = await Portfolio.findOne({uid: userID, name: args.name}, function(err, res) {
+        if (err) return console.log(err);
+        if(!res) throw new Error(errorName.NOT_FOUND);
+        res.stock_list = res.stock_list.map(obj => {
+            let rObj = obj;
+            rObj.stock = {};
+            rObj.stock.symbol = obj.symbol;
+            rObj.purchaseTime = rObj.purchaseTime.toJSON();
+            return rObj;
+        });
+        res.createdAt = res.createdAt.toJSON();
+        result = res;
     });
-    res.createdAt = res.createdAt.toJSON();
-    result = res;
-  });
-  console.log("Porfolio after modifiying time strings are: ",result );
-  if(!result) throw new Error(errorName.NOT_FOUND);
-  return result;
+    if(!result) throw new Error(errorName.NOT_FOUND);
+    return result;
 }
 
 let portfolioFieldResolvers = {
-  stock_list:  async (obj, args, context, info) => {
-    // Check the user is authenticated
-    if (!context.isAuth) throw new Error(errorName.ACCESS_DENIED);
-    // Get Portfolio by name
-    const name = obj.name;
-    // Checks for each element of the stock list
-    let parent_list = obj.stock_list;
-    // If no Portfolio was found throw and error
-    if(!parent_list) throw new Error(errorName.NOT_FOUND);
-    // For each stock populate its fields
-    let stock_list = [];
-    for (let stock_element in parent_list) {
-      stock_element = parseInt(stock_element);
-      let shares = parent_list[stock_element].shares;
-      let symbol = parent_list[stock_element].symbol;
-      let purchasePrice =  parent_list[stock_element].purchasePrice;
-      let purchaseTime =  parent_list[stock_element].purchaseTime;
-      let stock = {};
-      try {
-        stock = await axios.get(`https://financialmodelingprep.com/api/v3/quote/${symbol}`);
-        stock = stock.data[0];
-      } catch (err) {
-        console.log(err);
-      }
-      stock_list.push({stock: {
-        exchange: stock.exhange,
-        symbol: stock.symbol,
-        price: stock.price,
-        market_cap: stock.marketCap,
-        change: stock.change,
-        changes_percentage: stock.changesPercentage,
-        avg_volume: stock.avgVolume,
-      }, shares: shares , purchasePrice:purchasePrice,purchaseTime:purchaseTime});
-    }
-    return stock_list;
-  },
-  agregate: async (obj, args, context, info) => {
-    let stock_list = obj.stock_list;
-    let history_list = [];
-    for (i in stock_list) {
-      try {
-        let symbol = stock_list[i].symbol;
-        let stock = await axios.get(`https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}`);
-        history_list[i] = {history: stock.data.historical, symbol, amount: stock_list[i].amount}
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    let agregate = [];
-    for (i in history_list) {
-      for (j in history_list) {
-        agregate[j] = {
-          activeStock: 0,
-          value: 0,
-          individual
+    stock_list:  async (obj, args, context, info) => {
+        // Check the user is authenticated
+        if (!context.isAuth) throw new Error(errorName.ACCESS_DENIED);
+        // Get Portfolio by name
+        const name = obj.name;
+        // Checks for each element of the stock list
+        let parent_list = obj.stock_list;
+        // If no Portfolio was found throw and error
+        if(!parent_list) throw new Error(errorName.NOT_FOUND);
+        // For each stock populate its fields
+        let stock_list = [];
+        for (let stock_element in parent_list) {
+            stock_element = parseInt(stock_element);
+            let shares = parent_list[stock_element].shares;
+            let symbol = parent_list[stock_element].symbol;
+            let purchasePrice =  parent_list[stock_element].purchasePrice;
+            let purchaseTime =  parent_list[stock_element].purchaseTime;
+            let stock = {};
+            try {
+                stock = await axios.get(`https://financialmodelingprep.com/api/v3/quote/${symbol}`);
+                stock = stock.data[0];
+            } catch (err) {
+                console.log(err);
+            }
+            stock_list.push({stock: {
+                exchange: stock.exhange,
+                symbol: stock.symbol,
+                price: stock.price,
+                market_cap: stock.marketCap,
+                change: stock.change,
+                changes_percentage: stock.changesPercentage,
+                avg_volume: stock.avgVolume,
+            }, shares: shares , purchasePrice:purchasePrice,purchaseTime:purchaseTime});
         }
-      }
+        return stock_list;
+    },
+    agregate: async (obj, args, context, info) => {
+        let stock_list = obj.stock_list;
+        let history_list = [];
+        for (i in stock_list) {
+            try {
+                let symbol = stock_list[i].symbol;
+                let stock = await axios.get(`https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}`);
+                history_list[i] = {history: stock.data.historical, symbol, amount: stock_list[i].amount}
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        let agregate = [];
+        for (i in history_list) {
+            for (j in history_list) {
+                agregate[j] = {
+                    activeStock: 0,
+                    value: 0,
+                    individual
+                }
+            }
+        }
     }
-  }
 }
 
 // Delete Portfolio
@@ -189,17 +187,16 @@ let deletePortfolioQueryDef = `
 `;
 
 let deletePortfolioResolver = async (obj, args, context, info) => {
-  // Check the user is authenticated
-  if (!context.isAuth) throw new Error(errorName.ACCESS_DENIED);
-  let userID = context.uid;
-  //let userID = "sanicTheHedgehog";
-  // remove 1 document from db, return result if successful
-  let result = null;
-  let x = await Portfolio.findOneAndDelete({uid:userID, name: args.name}, function(err, item) {
-      result = item;
-  });
-  if (!result) throw new Error(errorName.NOT_FOUND);
-  return result;
+    // Check the user is authenticated
+    if (!context.isAuth) throw new Error(errorName.ACCESS_DENIED);
+    let userID = context.uid;
+    // remove 1 document from db, return result if successful
+    let result = null;
+    let x = await Portfolio.findOneAndDelete({uid:userID, name: args.name}, function(err, item) {
+        result = item;
+    });
+    if (!result) throw new Error(errorName.NOT_FOUND);
+    return result;
 }
 
 
@@ -213,28 +210,27 @@ let updatePortfolioQueryDef = `
 `;
 
 let updatePortfolioResolver = async (obj, args, context, info) => {
-  // Check the user is authenticated
-  if (!context.isAuth) throw new Error(errorName.ACCESS_DENIED);
-  //let userID = context.uid;
-  let userID = "sanicTheHedgehog";
-  // make sure portfolio with that name does exist for that username
-  let portfolio = await Portfolio.findOne(
+    // Check the user is authenticated
+    if (!context.isAuth) throw new Error(errorName.ACCESS_DENIED);
+    let userID = context.uid;
+    // make sure portfolio with that name does exist for that username
+    let portfolio = await Portfolio.findOne(
                             {uid: userID, name: args.name},
                             'name uid stock_list purchaseValue',
                             async function(err, doc) {
-    if (err) return console.log(err);
-  });
-  if (!portfolio) throw new Error(errorName.NOT_FOUND);
-  // uddate the portfolio object with the given stock list
-  let result = await Portfolio.findOneAndUpdate(
+        if (err) return console.log(err);
+    });
+    if (!portfolio) throw new Error(errorName.NOT_FOUND);
+    // uddate the portfolio object with the given stock list
+    let result = await Portfolio.findOneAndUpdate(
                             {uid: userID, name: args.name},
                             {stock_list: args.stock_list,purchaseValue:args.purchaseValue},
                             {upsert: false},
                             async function(err, doc) {
-    if (err) return console.log(err);
-  });
-  // return updated portfolio
-  return result;
+        if (err) return console.log(err);
+    });
+    // return updated portfolio
+    return result;
 }
 
 
@@ -245,10 +241,10 @@ let portfolioListQueryDef = `
 `;
 
 let portfolioListResolver = async (obj, args, context, info) => {
-  // Check the user is authenticated
-  if (!context.isAuth) throw new Error(errorName.ACCESS_DENIED);
-  // Fetch and return all portfolios that are associated with given uid
-  return await Portfolio.find({uid: context.uid});
+    // Check the user is authenticated
+    if (!context.isAuth) throw new Error(errorName.ACCESS_DENIED);
+    // Fetch and return all portfolios that are associated with given uid
+    return await Portfolio.find({uid: context.uid});
 }
 
 
