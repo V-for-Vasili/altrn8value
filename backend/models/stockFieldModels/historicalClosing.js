47fbcd064a14ae28ad30e85b508242b5bd88383f
Vasili Skurydzin <vasili.skurydzin@mail.utoronto.ca>
2020-04-11 20:14:08 -0400

    Caching historic data

--- backend/models/stockFieldModels/historicalClosing.js
+++ backend/models/stockFieldModels/historicalClosing.js
@@ -1,5 +1,6 @@
 // Imports
 const axios = require('axios');
+const {retrieveFromCache} = require('../../cache.js');
 
 
 let historicalClosingTypeDef = `
@@ -26,17 +27,34 @@ let historyResolver = async (obj, args, context, info) => {
     let response = {};
     try {
         if (timeseries == "line") {
-            response = await axios.get(`https://financialmodelingprep.com/api/v3/historical-price-full/${obj.symbol}?serietype=${timeseries}`);
-            response = response.data.historical;
-        } else if (timeseries in ["1min","5min","15min","30min","1hour"]) {
+            // get data from cache
+            // Cache for 60s
+            let key = `historicalPriceFull_line_${obj.symbol}`;
+            let url = `https://financialmodelingprep.com/api/v3/historical-price-full/${obj.symbol}?serietype=${timeseries}`;
+            response = await retrieveFromCache(key, url, 60);
+            response = response.historical;
+        } else if (timeseries == "1min") {
+            // We do not cache this
             response = await axios.get(`https://financialmodelingprep.com/api/v3/historical-chart/${timeseries}/${obj.symbol}`);
             response = response.data.historical;
+        } else if (timeseries in ["5min","15min","30min","1hour"]) {
+            // get data from cache
+            // Cache for 60s
+            let key = `historicalChart_${timeseries}_${obj.symbol}`;
+            let url = `https://financialmodelingprep.com/api/v3/historical-chart/${timeseries}/${obj.symbol}`;
+            response = await retrieveFromCache(key, url, 60);
+            response = response.historical;
         } else if (to && from) {
+            // We do not cache this because of key explosion
             response = await axios.get(`https://financialmodelingprep.com/api/v3/historical-price-full/${obj.symbol}?from=${from}&to=${to}`);
             response = response.data.historical;
         } else {
-            response = await axios.get(`https://financialmodelingprep.com/api/v3/historical-price-full/${obj.symbol}`);
-            response = response.data.historical;
+            // get data from cache
+            // Cache for 60s
+            let key = `historicalPriceFull_${obj.symbol}`;
+            let url = `https://financialmodelingprep.com/api/v3/historical-price-full/${obj.symbol}`;
+            response = await retrieveFromCache(key, url, 60);
+            response = response.historical;
         }
     } catch (err) {
         console.log(err);
