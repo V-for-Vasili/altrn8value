0684ec31a826183b4db48c49c4f9aa1acc9af706
V-for-Vasili <vasili.skurydzin@protonmail.com>
2020-10-10 22:51:02 -0400

    using certbot default directory for pem files

--- backend/server.js
+++ backend/server.js
@@ -199,8 +199,9 @@ app.get('/healthcheck/' ,async function (req, res) {
 
 // Run the server on PORT over https
 https.createServer({
-    key: fs.readFileSync('app_config/fullchain.pem'),
-    cert: fs.readFileSync('app_config/privkey.pem')
+    cert: fs.readFileSync('/etc/letsencrypt/live/altern8value.digital/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/altern8value.digital/privkey.pem'),
+    ca: fs.readFileSync('/etc/letsencrypt/live/altern8value.digital/chain.pem')
 }, app)
 .listen(PORT, function () {
     console.log(`Service running on port ${PORT}`);
